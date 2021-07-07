97496f874dd0560c22ba97aafc59b8f984bad76c
arhammusheer <arhammusheer@gmail.com>
2021-07-07 18:07:41 +0530

    added logo and protection

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,20 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Link,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Dashboard() {
-  const [Data, setData] = useState({});
+  const [data, setData] = useState({});
   useEffect(() => {
     axios
       .get("/api/user")
@@ -12,8 +23,45 @@ export default function Dashboard() {
       })
       .catch((err) => console.log(err));
   }, []);
-	if(!Data.isInServer){
-		return <Box>Sorry but you're not in the server</Box>
-	}
-  return JSON.stringify(Data);
+  if (!data.isInServer) {
+    return <NotInServerComponent user={data} />;
+  }
+  return JSON.stringify(data);
+}
+
+function NotInServerComponent({ user }) {
+  return (
+    <Flex align={"center"} justify={"center"} h={"100vh"}>
+      <Container>
+        <Stack direction={["column", "row"]} spacing={"2rem"}>
+          <Stack
+            align={"center"}
+            direction={["column", "row"]}
+            shadow={"xl"}
+            borderRadius={"xl"}
+            p={"5"}
+          >
+            <Avatar
+              name={"The Real Advisors"}
+              src={"./logo-500.png"}
+              bg={"transparent"}
+              size={"xl"}
+            />
+            <Box>
+              <Heading>The Real Advisors</Heading>
+              <Text>Connect with other people applying abroad.</Text>
+
+              <Button mt={3}
+                as={Link}
+                href={"https://discord.gg/jQUpuSmDwM"}
+                colorScheme={"blue"}
+              >
+                Join the discord server
+              </Button>
+            </Box>
+          </Stack>
+        </Stack>
+      </Container>
+    </Flex>
+  );
 }
