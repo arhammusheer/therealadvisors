f1d5444ecf6989e9730ce5bb45af391ac9264a98
arhammusheer <arhammusheer@gmail.com>
2021-07-12 17:25:08 +0530

    removec color

--- src/components/Verify.jsx
+++ src/components/Verify.jsx
@@ -5,7 +5,6 @@ import { useParams } from "react-router-dom";
 
 export default function Verify() {
   const [title, setTitle] = useState("Verifying your user");
-  const [color, setColor] = useState("black");
   const [message, setMessage] = useState(
     "Please wait while we process your request"
   );
@@ -21,13 +20,11 @@ export default function Verify() {
       })
         .then(() => {
           setIsLoaded(true);
-          setColor("green");
           setTitle(`Success: Role added`);
           setMessage(`A verification role has been added to your user`);
         })
         .catch(() => {
           setIsLoaded(true);
-          setColor("red");
           setTitle(`Error: Failed verfication`);
           setMessage(`The token provided is invalid`);
         });
@@ -38,12 +35,8 @@ export default function Verify() {
     <Flex align={"center"} height={"100vh"} justify={"center"}>
       <Spinner hidden={isLoaded} size={"xl"} />
       <Box>
-        <Heading size={"2xl"} color={color}>
-          {title}
-        </Heading>
-        <Text fontSize={"xl"} color={color}>
-          {message}
-        </Text>
+        <Heading size={"2xl"}>{title}</Heading>
+        <Text fontSize={"xl"}>{message}</Text>
       </Box>
     </Flex>
   );
